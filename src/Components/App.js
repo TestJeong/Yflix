c941d3614c68304697092e8e68d62564b2ac689a
TestJeong <jyunj3015@gmail.com>
2020-07-20 17:15:35 +0900

    Start Project
    - router 분류
    - styled-component 설정

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Router from "Components/Router";
+import GlobalStyles from "Components/GlobalStyles";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router />
+        <GlobalStyles />
+      </>
+    );
+  }
+}
+export default App;
+
+// Fragments(<>)는 내가 원하는 만큼 컴포넌트를 리턴 할 수 있게 해준다
