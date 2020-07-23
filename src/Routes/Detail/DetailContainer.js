66a59a134b3559ed9b97c982cbc7d155ac249f71
TestJeong <jyunj3015@gmail.com>
2020-07-23 22:22:19 +0900

    DetailContaner 완료

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,20 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { movieApi, tvApi } from "api";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"), //includes는 "/moive/"가 포함되어 있으면 true 아님 false를 반환한다
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -15,15 +23,29 @@ export default class extends React.Component {
       },
       history: { push },
     } = this.props;
+    const { isMovie } = this.state;
     const parsedId = parseInt(id); //문자열을 입력하면 NaN 값이 출력
     console.log(this.props);
     if (isNaN(parsedId)) {
       return push("/"); //NaN이며 push하고 함수를 종료
     }
+    let result;
+    try {
+      if (isMovie) {
+        ({ data: result } = await movieApi.movieDetail(parsedId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(parsedId));
+      }
+    } catch {
+      this.setState({ error: "Can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
+    }
   }
 
   render() {
     const { result, error, loading } = this.state;
+    console.log(this.props);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
