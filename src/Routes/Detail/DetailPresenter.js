f233aaf66b67fcb1eaefe92e9d61b2651d76be4c
TestJeong <jyunj3015@gmail.com>
2020-07-25 14:16:17 +0900

    Prop-Types 설정
    - Detail Presenter
    - Search Presnter

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,1 +1,13 @@
-export default () => "Detail";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-commponets";
+
+const DetailPresenter = ({ result, error, lodaing }) => null;
+
+DetailPresenter.prototype = {
+  result: PropTypes.object,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default DetailPresenter;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  searchTerm,
+  error,
+  loading,
+  handleSubmin,
+}) => null;
+
+SearchPresenter.prototype = {
+  movieResults: PropTypes.array,
+  tvResults: PropTypes.array,
+  error: PropTypes.string,
+  searchTerm: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  handleSubmin: PropTypes.func.isRequired,
+};
+
 export default () => "Search";
