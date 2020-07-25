a6ad4a1c4408e7a18f249aa6e99e74d06464758e
TestJeong <jyunj3015@gmail.com>
2020-07-25 16:19:28 +0900

    Prop-Types 수정

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,16 @@
-export default () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const TVPresenter = ({ topRated, popular, airingTodady, loading, error }) =>
+  null;
+
+TVPresenter.prototype = {
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  airingTodady: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default TVPresenter;
